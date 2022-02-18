742baf028b9ce347d98e9503ad608b867ffdc5ed
Jon Yardley <jonyardley@Jons-MacBook-Pro.local>
2022-02-18 18:31:44 +0000

    Try again

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
+    let name = "Jon Yardley!";
     let (count, set_count) = use_state(&cx, || 0);
     cx.render(rsx! (
         div {
-            h1 { "Hello World!" }
+            h1 { "Hello, {name}" }
             p { "Some body content" }
             h2 { "High-Five counter: {count}" }
             button { onclick: move |_| set_count(count + 1), "Up high!" }
