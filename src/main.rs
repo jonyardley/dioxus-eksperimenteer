67e11bb2da08fa5f4ece820a30eac04d69a1333b
Jon Yardley <jonyardley@Jons-MacBook-Pro.local>
2022-02-18 18:21:31 +0000

    Not sure what is going on, removing let

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    let name = "Jon Yardley!";
     let (count, set_count) = use_state(&cx, || 0);
     cx.render(rsx! (
         div {
-            h1 { "Hello, {name}" }
+            h1 { "Hello World!" }
             p { "Some body content" }
             h2 { "High-Five counter: {count}" }
             button { onclick: move |_| set_count(count + 1), "Up high!" }
