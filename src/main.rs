a8c5230bac63809d15ab269d8bd246b6141c342b
Jon Yardley <jonyardley@Jons-MacBook-Pro.local>
2022-03-18 15:42:24 +0000

    Upgrade dioxus

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,13 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    let name = "Jon Yardley!";
-    let (count, set_count) = use_state(&cx, || 0);
+    let mut count = use_state(&cx, || 0);
     cx.render(rsx! (
         div {
-            h1 { "Hello, {name}" }
-            p { "Some body content" }
-            h2 { "High-Five counter: {count}" }
-            button { onclick: move |_| set_count(count + 1), "Up high!" }
-            button { onclick: move |_| set_count(count - 1), "Down low!" }
+            h1 { "Hello, World!" }
+            h2 { "Counter value: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
         }
     ))
 }
