f104a44967306a7ffbaa2fc5a96616caaf08b62c
Jon Yardley <jonyardley@Jons-MacBook-Pro.local>
2022-04-05 16:31:50 +0100

    Updates and reformatting

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ fn app(cx: Scope) -> Element {
     let mut count = use_state(&cx, || 0);
     cx.render(rsx! (
         div {
-            h1 { "Hello, World!" }
-            h2 { "Counter value: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+        h1 { "Hello, World!" }
+        h2 { "Counter value: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
         }
     ))
 }
